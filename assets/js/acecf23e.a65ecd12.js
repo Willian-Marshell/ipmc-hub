e8c4d18dfdffe4fc5e54d813de71ad5a8e90101f
Willian-Marshell <Willian-Marshell@users.noreply.github.com>
2026-05-23 07:11:41 +0000

    deploy: 8a0a0f611e8c60cfbdf0279ad335c1aa5d12d60a

---
+++ assets/js/acecf23e.a65ecd12.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([["3220"],{1912(l){l.exports=JSON.parse('{"blogBasePath":"/my-blog/blog","blogTitle":"Blog","authorsListPath":"/my-blog/blog/authors"}')}}]);
